3cde47119454567d5aba5d785a6e95ffba39ec36
subhash <babumungar@gmail.com>
2023-06-08 17:58:03 +0530

    writing candiate funs

--- src/main/java/com/stg/repository/CandidateRepository.java
+++ src/main/java/com/stg/repository/CandidateRepository.java
@@ -1,10 +1,17 @@
 package com.stg.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.stg.model.Candidate;
 
-public interface CandidateRepository extends CrudRepository<Candidate, Integer>  {
-	
+public interface CandidateRepository extends CrudRepository<Candidate, Integer> {
+
+	public Candidate save(Optional<Candidate> candidateOb);
+
+	public Optional<Candidate> findByName(String name);
+
+	public Optional<Candidate> findByEmail(String email);
 
 }
